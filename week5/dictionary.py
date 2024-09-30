595d1021423c02cdfef1bd55fe40e494c257bcff
Lopez <avlopez2@cps.k12.il.us>
2024-09-30 11:58:29 -0500

    Week6_day1

---
+++ week5/dictionary.py
@@ -0,0 +1,31 @@
+# dictionary = a collection of { key : value } pairs ordered and changable. No duplicates 
+capitals = { "USA" : "Washignion DC ","India":"New Delhi","China":"Beijing"," Russia" : "Mascow" }
+ # print (dir(captials))
+ #print (help( capitals ))
+#help - gives you 
+print(capitals.get("USA"))
+#print (capitals.get (japan)) - doesnt exist 
+if capitals.get("Ruissa"):
+print("that capital exist ")
+else:
+print ("that capital doesn exist")
+capitals.update ({"Germany":"Berlin"})
+capitals.update ({"USA":"Detroits"})
+capitals.pop("China") # removes item 
+#remove the last item in the dctionary 
+capitals.popitem()
+#capitaisl.clear () will clear the dictornary 
+capitaisl.clear () 
+# returns the keys ans not the values 
+keys = capitals.keys()
+for every key in capitals.key ()
+print(keys)
+#returns all values nd not keys 
+values = capitals.values()
+print ( values)
+for value on capitals.values :
+print(values)
+capitals.items()
+print(items)
+for key, value in capitals.item ():
+print(f"{key}":"{value}")
