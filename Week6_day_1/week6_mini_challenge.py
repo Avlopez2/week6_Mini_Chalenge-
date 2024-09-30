12fce650acd340a6d263b759a895c006c3489856
Lopez <avlopez2@cps.k12.il.us>
2024-09-30 14:40:22 -0500

    first commit

--- Week6_day_1/week6_mini_challenge.py
+++ Week6_day_1/week6_mini_challenge.py
@@ -42,13 +42,22 @@ print(fruits)
 # surname: Jurgens
 # age: 35
 # occupation: Journalist
+
+my_dict = {
+ "name": "Karen",
+ "surname": "Jurgens",
+ "age": "35",
+ "occupation": "Journalist"
+ }
+print(my_dict)
 # The names of the keys and values must be equal to the ones indicated above.
 
 #   Dictionaries Practice #2
 # Use print to returns the second item of the list called points2, inside the following dictionary.
 # If the value 300 were to change in the future, the code should work the same to return the value at that same position. To do this, you must refer to the names of the keys and/or indexes as appropriate.
-# my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
-# print(my_dict[]) #Use dictionary indices to extract the second item of points2
+my_dict1 = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+
+print(my_dict1["points"]) #Use dictionary indices to extract the second item of points2
 
 # Dictionaries Practice #3
 # Update the information in our dictionary called my_dict (reassigning new values to the keys as appropriate), and add a new key called "country" (without a tilde). The new data is:
@@ -56,6 +65,10 @@ print(fruits)
 # surname: Jurgens
 # age: 36
 # occupation: Editor
-# country: Colombia
+# country: Colombiait 
 # to do this, you should not change the line of code already written, but update the values using dictionary methods.
 # my_dict = {"name":"Karen", "surname":"Jurgens", "age":35, "occupation":"Journalist"}
+
+my_dict.update({
+    "country": "Colombia"})
+print(my_dict)
