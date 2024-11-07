b7ee4b21b67442c614cb3d7a02776de48af00dcb
Lopez <avlopez2@cps.k12.il.us>
2024-11-07 11:59:23 -0600

    Week_10

---
+++ week_10.py/loops.py
@@ -0,0 +1,85 @@
+# while loops = execute some code WHILE some condition remain true
+# name= input('enter your name')
+# while name=="":
+#     print("You did not enter your name")
+#     name= input('enter your name:')
+
+# print(f"hello{name}")
+
+#iteration= Loops 
+#infinite loops- happens when the output goes on for ever and will lead your computer to crash, you use control c to stop it
+
+
+# age= int(input("enter your age"))
+
+# while age<0:
+#     print("age can not be negative")
+#     age= int(input("enter your age"))
+# print (f"you are {age} year old")
+
+# food= input( "entyer a food you like(q to quit):")
+# while not food=="q":
+#     print(f"you like{food}")
+#     food= input( "enter a food you like(q to quit):")
+# print("bye")
+
+
+
+# num= int(input('enter a number betwen 1-10:'))
+
+# while num< 1 or num >10:
+#    print(f"{num}is not vaild")
+#    num=int(input("enter a number between 1-10:"))
+
+# print(f"your number is {num}")
+
+
+# FOR loops = execute a block of code a fixed number of times.
+#you can iterate over a range string,sequence,etc. 
+
+# credit_card="123-3454-6554-7653"
+
+# for x in credit_card:
+#     print(x)
+
+# "continue skips the number or whatever you put"
+# for x in range(1,21):
+#     if x==13:
+#         continue
+#     else:
+#         print(x)
+
+# for x in range(1,21):
+#     if x==13:
+#         break # it stop counting once it gets to the number 
+#     else:
+#         print(x)
+
+#challenge 
+
+list_of_name=['John','paul','george','ringo']
+#if the name is "george", print 'george was found!"
+#otherwise,print george was not found! and print out the other name using a loop
+list_of_names2=['thanos','ironman','thor','hulk']
+#loop through the list_of_names2 an is the name is 'ironman' skip over it 
+#and print out the other names 
+
+# for name in list_of_name:
+#     if name=='george':
+#         print("george was found!")
+#     else:
+#         print ("george was not found!")
+#         print(name)
+
+
+# for name in list_of_names2: 
+#     if name=='ironman':
+#         continue
+#     print(name)
+    
+# for name in list_of_names2:
+#     if name=="hulk":
+#         update="wonder woman"
+#         print(update)
+#     else:
+#         print(name)
---
+++ week_9.py/week_9_day2.py
@@ -0,0 +1,30 @@
+####### Excerise Number 1 ########################
+fav_program = (input(" what is your favorite programing language?"))
+fav_program =="python"
+if fav_program == "python": 
+    print( "You love Python!")
+else:
+    print(" Different choice")
+########### Excerise Number 2 ##########################
+grade = int(input( " score from 0 to 100 "))
+if grade >= 90:
+    print(" Your correspodning grade is an A")
+elif grade >= 80 and grade < 90:
+      print(" Your correspodning grade is an B")
+elif grade >= 70 and grade < 80:
+      print(" Your correspodning grade is an C")
+elif grade  >= 60 and grade < 70:
+      print(" Your correspodnign grade is an D")
+else: 
+#       print ( " Your correspinding grade is an F")
+#################### Exerise Number 3 ################
+username = input(" what is uyour username?:")
+if username == "admin": 
+    print ( "Welcome admin!")
+else:
+#     print (" Wrong username , try again !")
+################ excerise number 4 ###############################
+A = [ 1, 4, 5, 7, 8, ]
+B = [ 5, 4, 7 ,8 ,1, ]
+print ( A == B )
+############# Excersie number 5 ####################
